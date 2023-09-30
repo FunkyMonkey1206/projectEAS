b9299602ee941b4e6bf1a855bd84fbcf2c95dee5
FunkyMonkey1206 <FunkyMonkey1206@users.noreply.github.com>
2023-10-01 00:17:11 +0900

    add create and clear buttons

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const body = document.querySelector('body');
 const container = document.createElement('div');
+const clearBtn = document.createElement('button');
+const createBtn = document.createElement('button');
 
 //grid size 16x16
 let gridSize = 256;
 
-
+clearBtn.classList.add('clearBtn');
+clearBtn.classList.add('createBtn');
 container.classList.add('container');
 body.appendChild(container);
-
+body.appendChild(clearBtn);
+body.appendChild(createBtn);
+clearBtn.textContent = 'clear';
+createBtn.textContent = 'create';
 
 //create 
 function createSquare(squareDiv) {
@@ -15,6 +21,8 @@ function createSquare(squareDiv) {
     squareDiv.setAttribute("class", "square");
     container.appendChild(squareDiv);
 }
+
+
 
 //create grids function
 function createGrid(size) {
@@ -26,7 +34,16 @@ function createGrid(size) {
     }
 }
 
+function clearGrid() {
+  var clearSqr = document.querySelectorAll('div.square')
+  clearSqr.forEach((sqr) => {
+    container.removeChild(sqr);
+  });
+};
+
 createGrid(gridSize);
+
+//create 
 
 const sqr = document.querySelectorAll('div.square');
 
@@ -36,4 +53,12 @@ sqr.forEach((sqr) => {
         sqr.classList.add('etch');
         // sqr.style.backgroundColor = 'blue';
     });
+});
+
+clearBtn.addEventListener('click', () => {
+    clearGrid();
+});
+
+createBtn.addEventListener('click', () => {
+    createGrid(gridSize);
 });
