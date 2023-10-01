bf125a3c9c7aced79af8b2fbab4467c4f4724e4c
FunkyMonkey1206 <FunkyMonkey1206@users.noreply.github.com>
2023-10-01 11:40:43 +0900

    Add feature to allow grid size change

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ const container = document.createElement('div');
 const clearBtn = document.createElement('button');
 const createBtn = document.createElement('button');
 
+
+
 //grid size 16x16
 let gridSize = 256;
+let side = 16;
 
 clearBtn.classList.add('clearBtn');
 clearBtn.classList.add('createBtn');
@@ -16,24 +19,32 @@ clearBtn.textContent = 'clear';
 createBtn.textContent = 'create';
 
 //create 
-function createSquare(squareDiv) {
+function createSquare(squareDiv, squareSide) {
     var squareDiv = document.createElement('div');
+    var squareHeight = 320 / Number(side);
+    var squareWidth = 320 / Number(side);
+
     squareDiv.setAttribute("class", "square");
+    squareDiv.style.width = `${squareWidth}px`;
+    squareDiv.style.height = `${squareHeight}px`;
     container.appendChild(squareDiv);
 }
 
 
 
-//create grids function
+//create grid function
 function createGrid(size) {
     let i = 1;
-
+    clearGrid()
     while(i <= size){
         createSquare(i);
         i++;
-    }
+    };
+    enableEtch();
+
 }
 
+//clear grid function
 function clearGrid() {
   var clearSqr = document.querySelectorAll('div.square')
   clearSqr.forEach((sqr) => {
@@ -44,21 +55,28 @@ function clearGrid() {
 createGrid(gridSize);
 
 //create 
-
-const sqr = document.querySelectorAll('div.square');
-
-sqr.forEach((sqr) => {
-
-    sqr.addEventListener('mouseover', ()  => {
-        sqr.classList.add('etch');
-        // sqr.style.backgroundColor = 'blue';
+function enableEtch() {
+    var sqr = document.querySelectorAll('div.square');
+    sqr.forEach((sqr) => {
+        sqr.addEventListener('mouseover', ()  => {
+            var rgbValue = Math.random() * 100;
+            sqr.classList.add('etch');
+            // sqr.style.cssText = `#${rgbValue}`;
+        });
     });
-});
+};
 
 clearBtn.addEventListener('click', () => {
     clearGrid();
 });
 
 createBtn.addEventListener('click', () => {
-    createGrid(gridSize);
+    side = prompt('how many squares per side?');
+    gridSize = Number(side) * Number(side);
+    createGrid(gridSize, Number(side));
 });
+
+
+
+
+//next step: figure out dynamic square re-sizing
